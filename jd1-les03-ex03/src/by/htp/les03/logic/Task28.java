28d09f4cf44ff77830725997218993b4651e9c84
mikhail300 <65508527+mikhail300@users.noreply.github.com>
2021-03-22 01:25:57 +0300

    Add files via upload

---
+++ jd1-les03-ex03/src/by/htp/les03/logic/Task28.java
@@ -0,0 +1,14 @@
+package by.htp.les03.logic;
+
+public class Task28 {
+// Составить программу перевода радианной меры угла в градусы, минуты и секунды.
+	public static void task() {
+		double a;
+		a = 5;
+		
+		double grad;
+//		grad = 
+
+	}
+
+}
