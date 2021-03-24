f8df15628a2ea23f03648082e921f6b81f4a9e04
mikhail300 <65508527+mikhail300@users.noreply.github.com>
2021-03-24 04:34:58 +0300

    Add files via upload

--- jd1-les03-ex03/src/by/htp/les03/logic/Task37.java
+++ jd1-les03-ex03/src/by/htp/les03/logic/Task37.java
@@ -19,9 +19,9 @@ public class Task37 {
 		
 		int N;
 		N = 11;
-		
-		System.out.println("\t" + "N = " + N);
+
 		System.out.println();
+		System.out.println("\t" + "Целое число N = " + N + " является четным двузначным числом.");
 		
 		if (100 > Math.abs(N) && Math.abs(N) > 9 && (N % 2) == 0) {
 			
@@ -43,10 +43,10 @@ public class Task37 {
 		
 		int N;
 		N = 1011;
-		
-		System.out.println("\t" + "N = " + N);
+
 		System.out.println();
-		
+		System.out.println("\t" + "Сумма двух первых цифр заданного четырехзначного числа " + N + " равна сумме двух его последних цифр.");
+				
 		int N0;
 		int N1;
 		int N2;
@@ -78,9 +78,9 @@ public class Task37 {
 		int N;
 		N = 111;
 		
-		System.out.println("\t" + "N = " + N);
 		System.out.println();
-		
+		System.out.println("\t" + "Сумма цифр данного трехзначного числа N = " + N + " является четным числом.");
+				
 		int N0;
 		int N1;
 		int N2;
@@ -112,18 +112,16 @@ public class Task37 {
 		
 		x = 6;
 		y = 5;
-		
-		System.out.println("\t" + "Точка с координатами x = " + x + ", y = " + y);
-		
+
 		double m;
 		double n;
 		
 		m = 2;
 		n = 5;
 		
-		System.out.println("\t" + "Прямая 1 x = " + m + ", прямая 2 x = " + n);
 		System.out.println();
-		
+		System.out.println("\t" + "Точка с координатами (x = " + x + ", y = " + y + ") принадлежит части плоскости, лежащей между прямыми x = " + m + ", x = " + n);
+
 		if (x >= m && n >= x) {
 			
 			System.out.println("\t" + "true");
@@ -145,9 +143,9 @@ public class Task37 {
 		int N;
 		N = 345;
 		
-		System.out.println("\t" + "Трехзначное число N = " + N);
 		System.out.println();
-		
+		System.out.println("\t" + "Квадрат заданного трехзначного числа " + N + " равен кубу суммы цифр этого числа.");
+				
 		int N0;
 		int N1;
 		int N2;
@@ -182,9 +180,9 @@ public class Task37 {
 		b = 2;
 		c = 3;
 
-		System.out.println("\t" + "Треугольник со сторонами " + a + ", " + b + ", " + c );
 		System.out.println();
-		
+		System.out.println("\t" + "Треугольник со сторонами a = " + a + ", b = " + b + ", c = " + c + " является равнобедренным.");
+				
 		if ((a == b && a != c) || (a == c && a != b) || (b == c && b != a) || (a == b && b == c)) {
 
 			System.out.println("\t" + "true");
@@ -202,18 +200,89 @@ public class Task37 {
 		System.out.println("37. Составить линейную программу, печатающую значение true,");
 		System.out.println("если указанное высказывание является истинным, и false — в противном случае:");
 		System.out.println("\t" + "7 Сумма каких-либо двух цифр заданного трехзначного натурального числа N равна третьей цифре.");
+
+		int N;
+		N = 342;
+		
+		System.out.println();
+		System.out.println("\t" + "Сумма каких-либо двух цифр заданного трехзначного натурального числа N = " + N + " равна третьей цифре.");
+				
+		int N0;
+		int N1;
+		int N2;
+				
+		N0 = N / 1 % 10;
+		N1 = N / 10 % 10;
+		N2 = N / 100 % 10;
+		
+		if (N0 + N1 == N2 || N0 + N2 == N1 || N2 + N1 == N0) {
+
+			System.out.println("\t" + "true");
+
+		}else{
+		
+			System.out.println("\t" + "false");
+
+		}
+		
+		
 		System.out.println();
 	}
+	
 	public static void part8() {
 		System.out.println("37. Составить линейную программу, печатающую значение true,");
 		System.out.println("если указанное высказывание является истинным, и false — в противном случае:");
 		System.out.println("\t" + "8 Заданное число N является степенью числа а (показатель степени может находиться в диапазоне от 0 до 4).");
+		
+		int N = 16;
+		int a = 2;
+		
+		System.out.println();
+		System.out.println("\t" + "Заданное число N = " + N + " является степенью числа a = " + a + " (показатель степени может находиться в диапазоне от 0 до 4).");
+				
+		if ((N == 1) || (N == a) || (N == a * a) || (N == Math.pow(a, 3)) || ((N == Math.pow(a, 4)))) {
+
+			System.out.println("\t" + "true");
+
+		}else{
+		
+			System.out.println("\t" + "false");
+
+		}
+		
 		System.out.println();
 	}
+	
 	public static void part9() {
 		System.out.println("37. Составить линейную программу, печатающую значение true,");
 		System.out.println("если указанное высказывание является истинным, и false — в противном случае:");
 		System.out.println("\t" + "9 График функции у = ах2 + bх+ с проходит через заданную точку с координатами (m, п).");
+		
+		double a;
+		double b;
+		double c;
+		double x;
+		double y;
+		
+		a = 1.0;
+		b = 3.0;
+		c = -4.0;
+		x = 1.0;
+		y = 0.0;
+
+		System.out.println();
+		System.out.println("\t" + "График функции у = " + a + "х^2 + " + b + "х+ " + c + " проходит через заданную точку с координатами (x = " + x + ", y = " + y + ")");
+				
+		if (y == a * x * x + b * x + c) {
+			
+			System.out.print("\t" + "true");
+			
+		}else{
+			
+			System.out.print("\t" + "false");
+			
+		}
+		
 		System.out.println();
 	}
 }
