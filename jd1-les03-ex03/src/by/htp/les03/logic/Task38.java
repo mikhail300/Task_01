a474210019b1973433eda98014d17300f42b92fe
mikhail300 <65508527+mikhail300@users.noreply.github.com>
2021-03-25 22:14:48 +0300

    Add files via upload

--- jd1-les03-ex03/src/by/htp/les03/logic/Task38.java
+++ jd1-les03-ex03/src/by/htp/les03/logic/Task38.java
@@ -10,6 +10,7 @@ public class Task38 {
 		int x = 2;
 		int y = 3;
 		
+		System.out.println();
 		System.out.println("\t" + "1 Точка с координатами (x = " + x + ", y = " + y + ") принадлежит закрашенной области.");
 		
 		if (y + Math.abs(x) <= 4 && y >= 0){
@@ -31,6 +32,7 @@ public class Task38 {
 		int x = 2;
 		int y = 3;
 		
+		System.out.println();
 		System.out.println("\t" + "2 Точка с координатами (x = " + x + ", y = " + y + ") принадлежит закрашенной области.");
 		
 		if ((y >= 0 && y <= 4 && x >= -2 && x <= 2) || (y >= -3 && y <= 0 && x >= -4 && x <= 4)){
@@ -46,8 +48,25 @@ public class Task38 {
 		}
 
 	public static void part3() {
-		System.out.println("\t" + "38. Для данных областей составить линейную программу, которая печатает true,");
+		System.out.println("38. Для данных областей составить линейную программу, которая печатает true,");
 		System.out.println("если точка с координатами (х, у) принадлежит закрашенной области, и false — в противном случае:");
 
+		int x = 2;
+		int y = 3;
+		
+		System.out.println();
+		System.out.println("\t" + "3 Точка с координатами (x = " + x + ", y = " + y + ") принадлежит закрашенной области.");
+		
+		
+		if (((x * x + y * y <= 16) && y >= 0 && x >= 0) || ((x * x + y * y <= 25) && y <= 0 && x >= 0)){
+		
+			System.out.println("\t" + "true");
+		
+		}else{
+		
+			System.out.println("\t" + "false");
+			
+		}
+		System.out.println();
 	}
 }
